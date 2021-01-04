87627cbc97af17c4ced4617a8d274cc7d12758b8
gregory.laksono01@homecredit.co.id <gregory.laksono01@homecredit.co.id>
2021-01-04 11:45:09 +0700

    init project

--- drools-dsl/src/main/java/gl/drools/model/Customer.java
+++ drools-dsl/src/main/java/gl/drools/model/Customer.java
@@ -1,4 +1,42 @@
 package gl.drools.model;
 
+import java.util.List;
+
 public class Customer {
+    private int point;
+    private String email;
+    private Discount discount;
+    private List<Order> orders;
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Discount getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
--- rules-release/src/main/java/app/config/DroolsConfiguration.java
+++ rules-release/src/main/java/app/config/DroolsConfiguration.java
@@ -1,7 +1,8 @@
-package config;
+package app.config;
 
 import org.drools.compiler.kproject.ReleaseIdImpl;
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
 import org.kie.api.builder.ReleaseId;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +12,19 @@ import org.springframework.context.annotation.Configuration;
 public class DroolsConfiguration {
 
     @Bean
-    KieContainer kieContainer(){
+    KieServices kieServices(){
+        System.setProperty("kie.maven.settings.custom","/Users/gregory.laksono01/.m2/settings.xml");
         KieServices ks = KieServices.get();
-        ReleaseId releaseId = new ReleaseIdImpl("gl-codework-drools","rules-central","LATEST");
-        KieContainer container = ks.newKieContainer(releaseId);
+        return ks;
+    }
+
+    @Bean
+    KieContainer kieContainer(KieServices ks){
+        ReleaseId releaseId = new ReleaseIdImpl("gl-codework-drools","rules-central","1.0-SNAPSHOT");
+        KieContainer container = ks.newKieContainer("release",releaseId);
+        KieScanner scanner = ks.newKieScanner(container);
+        scanner.start(500L);
         return container;
     }
+
 }
